a6e40b24e6317a62786a0085da651b6002ea4e3f
Matt Guthrie <matt@mkguthrie.com>
2019-07-03 08:43:06 -0700

    comments to js file

--- practice.js
+++ practice.js
@@ -1,4 +1,6 @@
 
+// Change title for all site section list items
+
 const elementsToChange = document.querySelectorAll('.site-section');
 
 for( let i = 0; i < elementsToChange.length; i++ ) {
@@ -8,6 +10,7 @@ for( let i = 0; i < elementsToChange.length; i++ ) {
     currentElement.style.fontWeight = '900';
 }
 
+// Grab input field and inject text into paragraph
 
 const inputField = document.querySelector('.input-to-copy');
 const paragraphNew = document.querySelector('.p-to-copy-to');
